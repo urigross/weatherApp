c312ca7c8ae42a16bbc5ee94c605d1cfb974db2c
urigross <urigross@gmail.com>
2021-12-23 13:08:59 +0200

    Added api key variable

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -3,9 +3,9 @@ import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs';
 
-
-const ROOT_URL = 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/212511?apikey=1O5x6iWGX2PXaKBdLiSGS82FD9V6HScH';
-//const ROOT_URL = 'https://api.npoint.io/6c9d1ba47b3a61ef639a';
+const API_KEY = '1O5x6iWGX2PXaKBdLiSGS82FD9V6HScH';
+//const ROOT_URL = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/212511?apikey=${API_KEY}`;
+const ROOT_URL = 'https://api.npoint.io/6c9d1ba47b3a61ef639a';
 
 var posts: any;
 @Injectable({
