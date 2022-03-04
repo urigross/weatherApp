09f53701fb20fd47053d2456ade85e4e81d1fef3
urigross <urigross@gmail.com>
2022-03-04 22:47:01 +0200

    Some remark cleanups on weather service

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -8,9 +8,6 @@ const apiKeys = [
   '1HTEFW2KdbvXG4CPLWGOxJFpP097Q58e',
   'pHmcrKbKZfiy2EAbQR1b1M80yG58k12G',
 ]
-//const API_KEY = '1O5x6iWGX2PXaKBdLiSGS82FD9V6HScH';
-//const API_KEY = '1HTEFW2KdbvXG4CPLWGOxJFpP097Q58e';
-//const API_KEY = 'pHmcrKbKZfiy2EAbQR1b1M80yG58k12G';
 
 //const AUTOCOMPLETE_MOCK_URL = `https://api.npoint.io/2ba8eb9f07495fd75a71`;
 //const FORECAST_URL = 'https://api.npoint.io/6c9d1ba47b3a61ef639a';
@@ -32,7 +29,6 @@ export class WeatherService {
   // HttpRequest returns Observable of object with cities forecast
   public  async getPosts(cityKey:string): Promise<Observable<any>> {
     const FORECAST_URL = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=${apiKeys[0]}&language=en-us&metric=true`;
-   // const FORECAST_URL = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=${API_KEY}`;
 
     return  await this.http.get(FORECAST_URL);
   }
